bad6563925b6f0456b05d8ae5b7c82990fc8cb46
Kpine-MO <chrispine.oduor@student.moringaschool.com>
2022-07-17 08:45:50 +0300

    initial commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,10 +4,18 @@ function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
 
+  function handleFirstName(event){
+    setFirstName(event.target.value)
+  }
+
+  function handleLastName(event){
+    setLastName(event.target.value)
+  }
+  console.log(firstName)
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" onChange={handleFirstName} value={firstName} />
+      <input type="text" onChange={handleLastName} value={lastName} />
       <button type="submit">Submit</button>
     </form>
   );
